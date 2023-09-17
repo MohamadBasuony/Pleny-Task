8e39e12c5a325076929d2741bd61c524f10c704d
Mohammad-Basuony <mohamad.basuony@optomni.com>
2023-09-17 22:59:50 +0300

    Initial Commit

---
+++ Pleny-Task/Pleny_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pleny_TaskApp.swift
+//  Pleny-Task
+//
+//  Created by Mohamad Basuony on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pleny_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
